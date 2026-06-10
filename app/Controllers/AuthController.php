a38bca3f6bd5dc93d1ee5a2e5b2181fcd9c18d94
RaihanMaulana-15913 <111202415913@mhs.dinus.ac.id>
2026-06-10 08:01:28 +0700

    first commit

---
+++ app/Controllers/AuthController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+use App\Models\UserModel;
+
+class AuthController extends BaseController
+{
+    protected $userModel;
+
+    function __construct()
+    {
+    helper('form');
+    $this->userModel = new UserModel();
+    }
+
+    public function login()
+    {
+        if ($this->request->getPost()) {
+            $rules = [
+                'username' => 'required|min_length[6]',
+                'password' => 'required|min_length[7]|numeric',
+            ];
+
+            if ($this->validate($rules)) {
+            $username = $this->request->getVar('username' );
+            $password = $this->request->getVar('password');
+
+            $dataUser = $this->userModel ->where(['username' => $username])->first();
+            //$dataUser = ['username' => 'Raihan', 'password' => '202cb962ac59075b964b07152d234b70', 'role' => 'admin']; // passw masih 123
+            if ($dataUser) {
+	            if (password_verify($password, $dataUser['password'])) { //password Bcrypt user admin : Raihan.Maulana = 1234567, user guest:Wielino.Septian = 7654321
+                session()->set([
+                    'username' => $dataUser['username'],
+                    'role' => $dataUser['role'],
+                    'isLoggedIn' => TRUE
+                ]);
+
+                return redirect()->to(base_url('/'));
+            } else {
+                session()->setFlashdata('failed', 'Username & Password Salah');
+                return redirect()->back();
+            }
+        } else {
+            session()->setFlashdata('failed', 'Username Tidak Ditemukan');
+            return redirect()->back();
+        }
+    } else {
+    session()->setFlashdata('failed', $this->validator->listErrors());
+    return redirect()->back();
+    }
+    } else {
+        return view('v_login');
+    }
+   }
+   
+   public function logout()
+   {
+        session()->destroy();
+        return redirect()->to('login');
+   }
+}
+
